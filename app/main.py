4b88624fa6e155068be1bb0e7971d5ce1695040d
Ericky Thierry <erickythierry@gmail.com>
2021-03-08 09:18:34 -0300

    update inicial

---
+++ app/main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from flask import Flask, request
+import youtube_dl
+
+app = Flask(__name__) 
+@app.route("/") 
+def get_info_yt():
+	url = request.args.get('url')
+	if url == '':
+		return "url vazia"
+	else:
+		try:
+			ydl_opts = {'noplaylist' : True}
+
+			with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+				meta = ydl.extract_info(
+					url, download=False)
+
+
+			titulo = meta['title']
+			tempo = meta['duration']
+			tempo = tempo/60
+			tempo = int(tempo)
+			id_video = meta['id']
+
+			return [titulo, tempo, id_video]
+
+		except Exception as e:
+			#print("resultado: "+str(e))
+			erro = str(e)
+
+			if "in your country" in erro:
+				print('erro de localização do video')
+				return 'erro, video indisponivel'
+
+			elif 'Too Many Requests' in erro:
+				print('bloqueio da  api')
+				return 'erro, ip block'
+
+			else:
+				return 'erro desconhecido'
