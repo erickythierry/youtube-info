b9d01531bb0ef9eeafefc7a38c0168611a971f19
Ericky Thierry <erickythierry@gmail.com>
2021-03-08 10:35:30 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,35 +5,35 @@ import youtube_dl
 
 def yt_dados(url):
 	try:
-			ydl_opts = {'noplaylist' : True}
+		ydl_opts = {'noplaylist' : True}
 
-			with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-				meta = ydl.extract_info(
-					url, download=False)
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+			meta = ydl.extract_info(
+				url, download=False)
 
 
-			titulo = meta['title']
-			tempo = meta['duration']
-			tempo = tempo/60
-			tempo = int(tempo)
-			id_video = meta['id']
+		titulo = meta['title']
+		tempo = meta['duration']
+		tempo = tempo/60
+		tempo = int(tempo)
+		id_video = meta['id']
 
-			return f'{titulo}\n{tempo}\n{id_video}'
+		return f'{titulo}\n{tempo}\n{id_video}'
 
-		except Exception as e:
-			#print("resultado: "+str(e))
-			erro = str(e)
+	except Exception as e:
+		#print("resultado: "+str(e))
+		erro = str(e)
 
-			if "in your country" in erro:
-				print('erro de localização do video')
-				return 'erro, video indisponivel'
+		if "in your country" in erro:
+			print('erro de localização do video')
+			return 'erro, video indisponivel'
 
-			elif 'Too Many Requests' in erro:
-				print('bloqueio da  api')
-				return 'erro, ip block'
+		elif 'Too Many Requests' in erro:
+			print('bloqueio da  api')
+			return 'erro, ip block'
 
-			else:
-				return 'erro desconhecido'
+		else:
+			return 'erro desconhecido'
 
 
 app = Flask(__name__) 
