700a173f7e959f86ec154c8d3c84fa9a480e70d8
Ericky Thierry <erickythierry@gmail.com>
2021-03-09 09:19:18 -0300

    uso de função fetch no index

--- app/static/script.js
+++ app/static/script.js
@@ -1,23 +1,37 @@
+const showData = (result)=>{
+    for(const campo in result){
+        console.log(campo)
+    };
+};
+
+
+
+
+
 function buscaYT() {
 	let inputlink = document.querySelector('input[id=link]');
 	let link = inputlink.value;
 	let url = 'https://t-youtube-info.herokuapp.com/info';
 	var data = new FormData();
 	data.append('link', link);
-	let xhr = new XMLHttpRequest();
-	xhr.open('POST', url, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			if (xhr.status = 200)
-				preencheCampos(xhr.responseText);
-				
-		}
-	}
-	xhr.send(data);
+
+	const options = {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'default',
+        body: data
+    };
+
+	fetch(url, options)
+    .then(response =>{ response.json()
+        .then( data => showData(data))
+    })
+    .catch(e => console.log('Deu Erro: '+ e,message))
+
 	var btnlimpar = document.getElementById("limpar");
 	btnlimpar.style.display = "inline";
 	  
-}
+};
 
 function preencheCampos(json) {
 
@@ -32,15 +46,15 @@ function preencheCampos(json) {
 	elemento_pai.appendChild(conteudo);
 
 
-}
+};
 
 function limpar(){
 	let textos = document.getElementsByClassName("resultado");
 	if (textos.length <=1 ) {
 		var btnlimpar = document.getElementById("limpar");
 		btnlimpar.style.display = "none";
-	}
+	};
 	for(var i=0; i<textos.length; i++) {
 		textos[-i].remove();
-	}
-}
+	};
+};
